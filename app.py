399021b9037911090e44ece9914b67f27e6a77db
Roshni Yadav <47268468+roshni-1@users.noreply.github.com>
2024-01-20 05:19:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ def webhook_handler():
         return jsonify({"message": "Webhook received successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+       
+        import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
